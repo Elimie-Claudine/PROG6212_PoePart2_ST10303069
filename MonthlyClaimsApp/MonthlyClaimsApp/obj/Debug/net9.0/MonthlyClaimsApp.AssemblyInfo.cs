81b7791d55ea80e4e9c3baecc7b38436eaad3961
Naomie <epremice04@gmail.com>
2025-10-20 11:52:55 +0200

    Fourth commit: I have put content in my About CMCS page

--- MonthlyClaimsApp/MonthlyClaimsApp/obj/Debug/net9.0/MonthlyClaimsApp.AssemblyInfo.cs
+++ MonthlyClaimsApp/MonthlyClaimsApp/obj/Debug/net9.0/MonthlyClaimsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonthlyClaimsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3575bdca2349af53a969971e0a193c9a5843f824")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb88427aa8e6b713438b11932bb6cabb67f4be2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonthlyClaimsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonthlyClaimsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PROG6212_PoePart2_ST10303069
+GitHub Link :  https://github.com/Elimie-Claudine/PROG6212_PoePart2_ST10303069.git 
+
+
+
+
+
+In POE Part 1, my feedback regarding the app was mostly about the fact that there was no user interface and just a template project, and also that there was not enough commits. 
+For Part 2, I made sure that my web app and well design with a user-friendly interface in each page. 
+I also made sure to push a minimum of 5 commit, so that the lecturer can view the progress of my work.
